d24586d20909def3a152b0499d7b8674883038bb
Luan Souza <luanscudeler@gmail.com>
2022-06-14 20:59:41 +0100

    add tests for Home page

--- pages/index.test.tsx
+++ pages/index.test.tsx
@@ -1,14 +1,92 @@
-import { render, screen } from '@testing-library/react'
-import Home from './index.page'
+import { fireEvent, render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { rest } from 'msw'
+import { todosResponseMock } from 'mocks/handlers'
+import { server } from 'mocks/server'
+import { SWRConfig } from 'swr'
+import * as api from '../pages/api'
+import Home, { phrases } from './index.page'
 
 describe('Home', () => {
-  it('renders a heading', () => {
-    render(<Home />)
+  const setupTest = () => {
+    return {
+      user: userEvent.setup(),
+      ...render(
+        <SWRConfig value={{ provider: () => new Map() }}>
+          <Home />
+        </SWRConfig>
+      ),
+    }
+  }
+
+  it('renders home page', async () => {
+    setupTest()
 
     const heading = screen.getByRole('heading', {
-      name: /welcome to next\.js!/i,
+      name: phrases.titleText,
+    })
+    const todoInputField = screen.getByRole('textbox', {
+      name: phrases.todoTitleLabel,
     })
 
     expect(heading).toBeInTheDocument()
+    expect(todoInputField).toBeInTheDocument()
+  })
+
+  it('renders list of todos', async () => {
+    setupTest()
+
+    expect(await screen.findByRole('list')).toBeInTheDocument()
+    expect(await screen.findAllByRole('listitem')).toHaveLength(
+      todosResponseMock.length
+    )
+  })
+
+  it('renders loading indicator', async () => {
+    jest.spyOn(api, 'fetcher').mockImplementationOnce(() => {
+      return new Promise((resolve) => {
+        setTimeout(() => resolve(''), 9999)
+      })
+    })
+
+    setupTest()
+
+    expect(await screen.findByText(phrases.loadingText)).toBeInTheDocument()
+  })
+
+  it('renders fetch error message', async () => {
+    server.use(
+      rest.get('/api/todos', (req, res, { json, status }) => {
+        return res.once(status(500))
+      })
+    )
+
+    setupTest()
+
+    expect(
+      await screen.findByText(phrases.fetchTodoErrorText)
+    ).toBeInTheDocument()
+  })
+
+  it('renders create todo error message', async () => {
+    server.use(
+      rest.post('/api/todo', (req, res, { json, status }) => {
+        return res.once(status(500))
+      })
+    )
+
+    const { user, debug } = setupTest()
+
+    const todoInputField = screen.getByRole('textbox', {
+      name: phrases.todoTitleLabel,
+    })
+
+    user.pointer({ target: todoInputField, keys: '[MouseLeft]' })
+    user.keyboard('Todo test')
+    user.keyboard('[Enter]')
+
+    expect(
+      await screen.findByText(phrases.createTodoErrorText)
+    ).toBeInTheDocument()
   })
 })
